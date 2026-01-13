d9b4553f70000067ea3894bf97add425ad7bd184
Alan Colver <accolver@gmail.com>
2026-01-12 18:04:18 -0700

    feat(nvim): add window nav keymaps and disable noice temporarily
    - Add <leader>h/j/k/l for intuitive window navigation
    - Move Lazy menu to <leader>L to free up <leader>l
    - Disable noice.nvim due to treesitter query incompatibility with Neovim 0.11.5

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Window navigation with <leader>h,j,k,l
+vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Move to window below" })
+vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Move to window above" })
+vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Move to right window" })
+
+-- Move Lazy to <leader>L (capital L)
+vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
 return {
   {
     "folke/snacks.nvim",
---
+++ config/nvim/lua/plugins/noice-disable.lua
@@ -0,0 +1,5 @@
+-- Disable noice.nvim due to treesitter query incompatibility with Neovim 0.11.5
+-- Remove this file after updating noice.nvim
+return {
+  { "folke/noice.nvim", enabled = false },
+}
